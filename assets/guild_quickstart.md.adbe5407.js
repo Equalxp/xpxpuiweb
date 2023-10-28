8d2e107f48544dc167d05f1460e4a8dbed4bccd6
equalxp <2106704492@qq.com>
2023-10-28 23:04:00 +0800

    first-commit

---
+++ assets/guild_quickstart.md.adbe5407.js
@@ -0,0 +1,17 @@
+import{_ as s,c as a,o as n,Q as e}from"./chunks/framework.48ea42b7.js";const h=JSON.parse('{"title":"快速开始","description":"","frontmatter":{},"headers":[],"relativePath":"guild/quickstart.md","filePath":"guild/quickstart.md"}'),t={name:"guild/quickstart.md"},p=e(`<h1 id="快速开始" tabindex="-1">快速开始 <a class="header-anchor" href="#快速开始" aria-label="Permalink to &quot;快速开始&quot;">​</a></h1><p>本节将介绍如何在项目中使用 XpxpUI</p><h2 id="用法" tabindex="-1">用法 <a class="header-anchor" href="#用法" aria-label="Permalink to &quot;用法&quot;">​</a></h2><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki github-dark vp-code-dark"><code><span class="line"><span style="color:#e1e4e8;">&lt;template&gt;</span></span>
+<span class="line"><span style="color:#e1e4e8;">  &lt;xp-button&gt;按钮&lt;/xp-button&gt;</span></span>
+<span class="line"><span style="color:#e1e4e8;">&lt;/template&gt;</span></span>
+<span class="line"><span style="color:#e1e4e8;"></span></span>
+<span class="line"><span style="color:#e1e4e8;">&lt;script setup&gt;</span></span>
+<span class="line"><span style="color:#e1e4e8;"></span></span>
+<span class="line"><span style="color:#e1e4e8;">import { XpButton } from &#39;xpxpui&#39;</span></span>
+<span class="line"><span style="color:#e1e4e8;"></span></span>
+<span class="line"><span style="color:#e1e4e8;">&lt;/script&gt;</span></span></code></pre><pre class="shiki github-light vp-code-light"><code><span class="line"><span style="color:#24292e;">&lt;template&gt;</span></span>
+<span class="line"><span style="color:#24292e;">  &lt;xp-button&gt;按钮&lt;/xp-button&gt;</span></span>
+<span class="line"><span style="color:#24292e;">&lt;/template&gt;</span></span>
+<span class="line"><span style="color:#24292e;"></span></span>
+<span class="line"><span style="color:#24292e;">&lt;script setup&gt;</span></span>
+<span class="line"><span style="color:#24292e;"></span></span>
+<span class="line"><span style="color:#24292e;">import { XpButton } from &#39;xpxpui&#39;</span></span>
+<span class="line"><span style="color:#24292e;"></span></span>
+<span class="line"><span style="color:#24292e;">&lt;/script&gt;</span></span></code></pre></div>`,4),l=[p];function o(c,i,r,d,u,_){return n(),a("div",null,l)}const m=s(t,[["render",o]]);export{h as __pageData,m as default};
